70b9607abba3ea1581ccb1e617805894b67ef21a
DavidValles <davs93@hotmail.com>
2015-08-04 23:34:46 -0500

    All clases work together, Class contact requires address and phoneN

--- contact.rb
+++ contact.rb
@@ -1,12 +1,14 @@
 
 require "./phone_number"
+require "./address"
 
 class Contact
 	attr_writer :first_name, :middle_name, :last_name
-	attr_reader :phone_numbers
+	attr_reader :phone_numbers, :addresses
 
 	def initialize
 		@phone_numbers = []
+		@addresses = []
 	end
 
 	def add_phone_number(kind, number)
@@ -15,6 +17,19 @@ class Contact
 		phone_number.number = number
 		phone_numbers.push(phone_number)
 	end
+
+	def add_address(kind, street_1, street_2, city, state, postal_code)
+		address = Address.new
+		address.kind = kind
+		address.street_1 = street_1
+		address.street_2 = street_2
+		address.city = city
+		address.state = state
+		address.postal_code = postal_code
+		addresses.push(address)
+
+	end
+
 
 	def first_name
 		@first_name
@@ -76,12 +91,20 @@ class Contact
 		phone_numbers.each {|phone_number| puts phone_number}
 	end
 
+	def print_addresses
+		puts "Addresses"
+		addresses.each {|address| puts address.to_s('short')}
+	end
+
+
 end
 
 david = Contact.new
 david.first_name = "David"
 david.last_name = "Valles"
 david.add_phone_number("Home", "123-456-789")
+david.add_address("Home", "123 Main St.", "", "Portland", "OR", "12345")
 puts david.full_name
 david.print_phone_numbers
+david.print_addresses
 
