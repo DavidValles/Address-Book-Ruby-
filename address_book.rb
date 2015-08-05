8f910170e19e77e11b7d738534a66d6ed1de7f3b
DavidValles <davs93@hotmail.com>
2015-08-05 15:23:10 -0500

    method search by phoneN and address ready

--- address_book.rb
+++ address_book.rb
@@ -8,22 +8,54 @@ class AddressBook
 		@contacts = []
 	end
 
+	def print_results(search, results)
+	  puts search
+	  results.each do |contact|
+	    puts contact.to_s('full_name')
+	    contact.print_phone_numbers
+	    contact.print_addresses
+	    puts "\n"
+	  end
+	end
+
 	def find_by_name(name)
-  results = []
-  search = name.downcase
-  contacts.each do |contact|
-    if contact.full_name.downcase.include?(search)
-      results.push(contact)
-    end
-  end
-  puts "Name search results (#{search})"
-  results.each do |contact|
-    puts contact.to_s('full_name')
-    contact.print_phone_numbers
-    contact.print_addresses
-    puts "\n"
-  end
-end 
+	  results = []
+	  search = name.downcase
+	  contacts.each do |contact|
+	    if contact.full_name.downcase.include?(search)
+	      results.push(contact)
+	    end
+	  end
+	 print_results("Name search results (#{search})", results)
+	end 
+
+	def find_by_phone_number(number)
+	  results = []
+	  search = number.gsub("-", "")
+	  contacts.each do |contact|
+	    contact.phone_numbers.each do |phone_number|
+	      if phone_number.number.gsub("-", "").include?(search)
+	        results.push(contact) unless results.include?(contact)
+	      end
+	    end
+	  end
+	  print_results("Phone search results (#{search})", results)
+	end
+
+
+	def find_by_address(query)
+	  results = []
+	  search = query.downcase
+	  contacts.each do |contact|
+	    contact.addresses.each do |address|
+	      if address.to_s('long').downcase.include?(search)
+	        results.push(contact) unless results.include?(contact)
+	      end
+	    end
+	  end
+	  print_results("Address search results (#{search})", results)
+	end
+
 
 	def print_contacts_list
 		contacts.each do |contact|
@@ -50,4 +82,6 @@ address_book.contacts.push(david)
 address_book.contacts.push(daniel)
 #address_book.print_contacts_list
 
-address_book.find_by_name("D")
+#address_book.find_by_name("D")
+
+address_book.find_by_phone_number("123")
