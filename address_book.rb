b77ce86b484183f4c6272938e361c88db37d42cf
DavidValles <davs93@hotmail.com>
2015-08-06 17:03:14 -0600

    END

--- address_book.rb
+++ address_book.rb
@@ -1,4 +1,6 @@
 require "./contact.rb"
+require "./contact"
+require "yaml"
 
 class AddressBook
 
@@ -6,26 +8,48 @@ class AddressBook
 
 	def initialize
 		@contacts = []
+		open()
 	end
 
-	def run
-	  loop do
-	    puts "Address Book"
-	    puts "a: Add Contact"
-	    puts "p: Print Address Book"
-	    puts "e: Exit"
-	    print "Enter your choice: "
-	    input = gets.chomp.downcase
-	    case input
-	    when 'a'
-	      add_contact
-	    when 'p'
-	      print_contact_list
-	    when 'e'
-	      break  
+	def open
+	    if File.exist?("contacts.yml")
+	      @contacts = YAML.load_file("contacts.yml")
 	    end
-	  end
-	end
+	 end
+
+	 def save
+	    File.open("contacts.yml", "w") do |file|
+	      file.write(contacts.to_yaml)
+	    end
+	 end
+
+	def run
+	    loop do
+	      puts "Address Book"
+	      puts "a: Add Contact"
+	      puts "p: Print Address Book"
+	      puts "s: Search"
+	      puts "e: Exit"
+	      print "Enter your choice: "
+	      input = gets.chomp.downcase
+	      case input
+	      when 'a'
+	        add_contact
+	      when 'p'
+	        print_contact_list
+	      when 's'
+	        print "Search term: "
+	        search = gets.chomp
+	        find_by_name(search)
+	        find_by_phone_number(search)
+	        find_by_address(search)
+	      when 'e'
+	        save()
+	        break  
+	      end
+	      puts "\n"
+	    end
+	 end
 
 	def add_contact
 	  contact = Contact.new
